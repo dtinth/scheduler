f6b724c99f97a67312483f07d299fc01234e4cab
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2013-11-20 11:46:49 +0700

    add course editor component

---
+++ js/editor.js
@@ -0,0 +1,48 @@
+
+angular.module('scheduler.editor', ['ui.utils'])
+  .controller('SectionController', function($scope) {
+    $scope.addPeriod = function() {
+      $scope.section.periods.push({
+        day: 1, start: '', finish: '', place: ''
+      })
+    }
+    $scope.removePeriod = function(period) {
+      $scope.section.periods.splice($scope.section.periods.indexOf(period), 1)
+    }
+  })
+  .controller('EditorController', function($scope) {
+    $scope.addSection = function() {
+      $scope.data.sections.push({
+        sectionNo: '',
+        instructor: '',
+        periods: [
+          { day: 1, start: '', finish: '', place: '' }
+        ]
+      })
+    }
+  })
+  .controller('EditorTestController', function($scope) {
+    $scope.data = {
+      courseId: '01204111',
+      courseName: 'Computers and Programming',
+      sections: [
+        {
+          sectionNo: '450',
+          instructor: 'Tyghe',
+          periods: [
+            { day: 1, start: '13:00', finish: '14:30', place: '17201' },
+            { day: 5, start: '13:30', finish: '15:00', place: '0503' }
+          ]
+        },
+        {
+          sectionNo: '451',
+          instructor: 'Siriporn',
+          periods: [
+            { day: 1, start: '13:00', finish: '14:30', place: '0202' },
+            { day: 5, start: '13:30', finish: '15:00', place: '0203' }
+          ]
+        }
+      ]
+    }
+  })
+
---
+++ js/scheduler.js
@@ -0,0 +1,72 @@
+
+angular.module('scheduler', ['scheduler.editor'])
+  .controller('MainController', function($scope) {
+    $scope.courses = [
+      {
+        courseId: '01204111',
+        courseName: 'Computers and Programming',
+        sections: [
+          {
+            sectionNo: '450',
+            instructor: 'Tyghe',
+            periods: [
+              { day: 1, start: '13:00', finish: '14:30', place: '17201' },
+              { day: 5, start: '13:30', finish: '15:00', place: '0503' }
+            ]
+          },
+          {
+            sectionNo: '451',
+            instructor: 'Siriporn',
+            periods: [
+              { day: 1, start: '13:00', finish: '14:30', place: '0202' },
+              { day: 5, start: '13:30', finish: '15:00', place: '0203' }
+            ]
+          }
+        ]
+      },
+      {
+        courseId: '01204112',
+        courseName: 'Computers and Programming XXX',
+        sections: [
+          {
+            sectionNo: '450',
+            instructor: 'Tyghe',
+            periods: [
+              { day: 1, start: '13:00', finish: '14:30', place: '17201' },
+              { day: 5, start: '13:30', finish: '15:00', place: '0503' }
+            ]
+          },
+          {
+            sectionNo: '451',
+            instructor: 'Siriporn',
+            periods: [
+              { day: 1, start: '13:00', finish: '14:30', place: '0202' },
+              { day: 5, start: '13:30', finish: '15:00', place: '0203' }
+            ]
+          }
+        ]
+      },
+      {
+        courseId: '01204113',
+        courseName: 'Computers and Programming YYY',
+        sections: [
+          {
+            sectionNo: '450',
+            instructor: 'Tyghe',
+            periods: [
+              { day: 1, start: '13:00', finish: '14:30', place: '17201' },
+              { day: 5, start: '13:30', finish: '15:00', place: '0503' }
+            ]
+          },
+          {
+            sectionNo: '451',
+            instructor: 'Siriporn',
+            periods: [
+              { day: 1, start: '13:00', finish: '14:30', place: '0202' },
+              { day: 5, start: '13:30', finish: '15:00', place: '0203' }
+            ]
+          }
+        ]
+      }
+    ]
+  })
