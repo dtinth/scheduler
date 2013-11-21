7ee486180e1eabc25f3fa090a35827cfdd391e9f
beammagic <beammagics@gmail.com>
2013-11-21 16:00:11 +0700

    Make sidebar dynamic with data

--- js/scheduler.js
+++ js/scheduler.js
@@ -65,6 +65,14 @@ angular.module('scheduler', ['scheduler.editor'])
               { day: 1, start: '13:00', finish: '14:30', place: '0202' },
               { day: 5, start: '13:30', finish: '15:00', place: '0203' }
             ]
+          },
+          {
+            sectionNo: '452',
+            instructor: 'Siriporn',
+            periods: [
+              { day: 1, start: '13:00', finish: '14:30', place: '0202' },
+              { day: 5, start: '13:30', finish: '15:00', place: '0203' }
+            ]
           }
         ]
       }
